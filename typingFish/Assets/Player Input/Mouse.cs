f21dee3de49d13de01658a199d8558ada48385e5
jill <jillian.chapin@mymail.champlain.edu>
2026-08-31 14:03:09 -0400

    Mouse State
    Mouse toggles between hook or standard mouse state

--- typingFish/Assets/Player Input/Mouse.cs
+++ typingFish/Assets/Player Input/Mouse.cs
@@ -1,10 +1,55 @@
 using UnityEngine;
 
+public enum MouseState
+{
+    NULL = -1,
+    MOUSE,
+    HOOK,
+    NUM_STATES
+}
+
+
+
 public class Mouse : MonoBehaviour, IStateable
 {
     public static Mouse instance;
-    [SerializeField] float moveSpeed;   
-    public void HandleState() { }
+    MouseState state;
+    public void SetState(MouseState newState)
+    {
+        state = newState;
+
+        switch (state)
+        {
+            case MouseState.HOOK:
+                Cursor.visible = false;
+                hook.SetActive(true);
+                break;
+            case MouseState.MOUSE:
+                Cursor.visible = true;
+                hook.SetActive(false);
+                break;
+        }
+
+    }
+
+    [Header("Hook State")]
+    [SerializeField] GameObject hook;
+    [SerializeField] float hookMoveSpeed;
+    public void HandleState()
+    {
+        switch (GameManager.instance.GetState())
+        {
+            case GameState.CASTING:
+            case GameState.FISHING:
+                SetState(MouseState.HOOK);
+                break;
+
+            case GameState.TYPING:
+                SetState(MouseState.MOUSE);
+                break;
+        }
+
+    }
 
     void Start()
     {
@@ -15,14 +60,15 @@ public class Mouse : MonoBehaviour, IStateable
     // Update is called once per frame
     void Update()
     {
-        FollowMouse();
+        if(state == MouseState.HOOK)
+            HookFollowMouse();
     }
 
-    void FollowMouse()
+    void HookFollowMouse()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition = new Vector3(mousePosition.x, mousePosition.y, 0f);
-        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, hookMoveSpeed);
     }
 
 	private void OnTriggerEnter2D(Collider2D collision)
